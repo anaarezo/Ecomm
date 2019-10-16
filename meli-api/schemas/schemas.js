af036c70f897f36f1cd65a67cd1afc90d8dbc3df
Ana Laura Arezo E Silva Rodrigues <ana.rodrigues@grupoboticario.com.br>
2019-10-16 11:29:19 -0300

    Fix

--- meli-api/schemas/schemas.js
+++
@@ -1,9 +0,0 @@
-const ItemsSchema = require('./itemsSchema');
-const SearchSchema = require('./searchSchema');
-
-const schemas = {
-  ItemsSchema: ItemsSchema,
-  SearchSchema: SearchSchema
-};
-
-module.exports = schemas;
